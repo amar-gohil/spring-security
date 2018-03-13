aa6b7805e5921effe53ff807e3f2ddf81f5eeaaf
amar-gohil <amar7gohil@gmail.com>
2018-03-13 17:36:53 +0530

    Add files via upload

--- src/main/java/com/security/security/OAuth2SecurityConfiguration.java
+++ src/main/java/com/security/security/OAuth2SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
@@ -22,14 +23,27 @@ public class OAuth2SecurityConfiguration extends WebSecurityConfigurerAdapter {
  
     @Autowired
     private ClientDetailsService clientDetailsService;
-     
+   
+          
+//    @Autowired
+//    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//        .withUser("bill").password("abc123").roles("ADMIN").and()
+//        .withUser("bob").password("abc123").roles("USER");
+//    }
+    
+
     @Autowired
-    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-        .withUser("bill").password("abc123").roles("ADMIN").and()
-        .withUser("bob").password("abc123").roles("USER");
+	private UserDetailsService userDetailsService;
+
+    
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
     }
- 
+    
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
